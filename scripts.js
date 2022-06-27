32c463e9714a0cf5754d5c8bd674bb96e9fac829
NatalliaPahosava <mulato4ka114@gmail.com>
2022-06-27 13:48:29 -0400

    menu for restaurant

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('.menu-items')
+
+// go get me some food from API
+fetch('https://codice-boca.web.app/menu')
+  .then(myRes => myRes.json()) // handling promise then formatting to json
+  .then(data => {
+    let html = ''
+
+    data.forEach(eachItem => {
+      let htmlItem = `<div class="menu-items__item">
+      <h3>${eachItem.title}</h3>
+      <p>${eachItem.description}</p>
+    </div>`
+      console.log('each item => ', eachItem)
+
+      html += htmlItem
+    })
+    menu.innerHTML = html
+  })
+  .catch(err => console.error(err))
